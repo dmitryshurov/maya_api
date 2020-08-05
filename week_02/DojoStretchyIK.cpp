259cf84e4fa9973c5e65549ff74e48d98455b5f4
d.shurov <d.shurov@cgfww.ru>
2020-08-05 21:19:48 +0300

    My own implementation of StretchyIK, which is not very good, I suppose

--- week_02/DojoStretchyIK.cpp
+++ week_02/DojoStretchyIK.cpp
@@ -2,9 +2,12 @@
 
 #include <maya/MFnNumericAttribute.h>
 #include <maya/MFnMatrixAttribute.h>
+#include <maya/MMatrix.h>
+#include <maya/MVector.h>
 #include <maya/MGlobal.h>
 
 #define MAYA_PARM(name)  // A dummy to organize parameters in the code
+#define AFFECTS(name)    // A dummy to organize parameter affects in the code
 
 MTypeId DojoStretchyIK::typeID(0x5D2C2 );
 
@@ -51,16 +54,14 @@ MStatus DojoStretchyIK::initialize()
     }
 
     MAYA_PARM(upScale) {
-        upScale = numFn.create("Up_Scale", "ups", MFnNumericData::kDouble);
-
+        upScale = numFn.create("Up_Scale", "ups", MFnNumericData::kDouble, 1.0);
         numFn.setKeyable(false);
         numFn.setWritable(false);
         numFn.setStorable(false);
         addAttribute(upScale);
     }
     MAYA_PARM(downScale) {
-        downScale = numFn.create("Down_Scale", "dns", MFnNumericData::kDouble);
-
+        downScale = numFn.create("Down_Scale", "dns", MFnNumericData::kDouble, 1.0);
         numFn.setKeyable(false);
         numFn.setWritable(false);
         numFn.setStorable(false);
@@ -68,71 +69,161 @@ MStatus DojoStretchyIK::initialize()
     }
 
     MAYA_PARM(upInitLength) {
-        upInitLength = numFn.create("Up_Init_Length", "upis", MFnNumericData::kDouble);
-
+        upInitLength = numFn.create("Up_Init_Length", "upis", MFnNumericData::kDouble, 0);
         numFn.setKeyable(true);
         numFn.setWritable(true);
         numFn.setStorable(true);
+        numFn.setMin(0.001);
         addAttribute(upInitLength);
     }
     MAYA_PARM(downInitLength) {
-        downInitLength = numFn.create("Down_Init_Length", "dnis", MFnNumericData::kDouble);
-
+        downInitLength = numFn.create("Down_Init_Length", "dnis", MFnNumericData::kDouble, 0);
         numFn.setKeyable(true);
         numFn.setWritable(true);
         numFn.setStorable(true);
+        numFn.setMin(0.001);
         addAttribute(downInitLength);
     }
     MAYA_PARM(globalScale) {
-        globalScale = numFn.create("Global_Scale", "gls", MFnNumericData::kDouble);
-
+        globalScale = numFn.create("Global_Scale", "gls", MFnNumericData::kDouble, 1.0);
         numFn.setKeyable(true);
         numFn.setWritable(true);
         numFn.setStorable(true);
+        numFn.setMin(0.001);
         addAttribute(globalScale);
     }
     MAYA_PARM(poleVectorLock) {
-        poleVectorLock = numFn.create("Pole_Vector_Lock", "pvl", MFnNumericData::kDouble);
-
+        poleVectorLock = numFn.create("Pole_Vector_Lock", "pvl", MFnNumericData::kDouble, 0.0);
         numFn.setKeyable(true);
         numFn.setWritable(true);
         numFn.setStorable(true);
+        numFn.setMin(0.0);
+        numFn.setMax(1.0);
         addAttribute(poleVectorLock);
     }
     MAYA_PARM(stretch) {
-        stretch = numFn.create("Stretch", "str", MFnNumericData::kDouble);
-
+        stretch = numFn.create("Stretch", "str", MFnNumericData::kDouble, 0.0);
         numFn.setKeyable(true);
         numFn.setWritable(true);
         numFn.setStorable(true);
+        numFn.setMin(0.0);
+        numFn.setMax(1.0);
         addAttribute(stretch);
     }
     MAYA_PARM(slide) {
-        slide = numFn.create("Slide", "sld", MFnNumericData::kDouble);
-
+        slide = numFn.create("Slide", "sld", MFnNumericData::kDouble, 0.5);
         numFn.setKeyable(true);
         numFn.setWritable(true);
         numFn.setStorable(true);
+        numFn.setMin(0.0);
+        numFn.setMax(1.0);
         addAttribute(slide);
     }
 
-    attributeAffects(slide, upScale);
-    attributeAffects(slide, downScale);
+    AFFECTS(affects_up) {
+        attributeAffects(startMatrix, upScale);
+        attributeAffects(endMatrix, upScale);
+        attributeAffects(poleVectorMatrix, upScale);
+
+        attributeAffects(upInitLength, upScale);
+        attributeAffects(downInitLength, upScale);
+        attributeAffects(globalScale, upScale);
+        attributeAffects(poleVectorLock, upScale);
+        attributeAffects(stretch, upScale);
+        attributeAffects(slide, upScale);
+    }
+    AFFECTS(affects_down) {
+        attributeAffects(startMatrix, downScale);
+        attributeAffects(endMatrix, downScale);
+        attributeAffects(poleVectorMatrix, downScale);
+
+        attributeAffects(upInitLength, downScale);
+        attributeAffects(downInitLength, downScale);
+        attributeAffects(globalScale, downScale);
+        attributeAffects(poleVectorLock, downScale);
+        attributeAffects(stretch, downScale);
+        attributeAffects(slide, downScale);
+    }
 
     return MStatus();
 }
 
 MStatus DojoStretchyIK::compute(const MPlug &plug, MDataBlock &data)
 {
-//    if (plug == outputU)
-//    {
-//        double inLen = data.inputValue(inputLength).asDouble();
-//        MFnNurbsCurve nurbsCrv(data.inputValue(inputCurve).asNurbsCurve());
-//        double outVal = nurbsCrv.findParamFromLength(inLen);
-//
-//        data.outputValue(outputU).set(outVal);
-//        data.outputValue(outputU).setClean();
-//    }
+    if (plug == upScale || plug == downScale)
+    {
+        MMatrix startMatrixV = data.inputValue(startMatrix).asMatrix();
+        MMatrix endMatrixV = data.inputValue(endMatrix).asMatrix();
+        MMatrix poleVectorMatrixV = data.inputValue(poleVectorMatrix).asMatrix();
+
+        double upInitLengthV = data.inputValue(upInitLength).asDouble();
+        double downInitLengthV = data.inputValue(downInitLength).asDouble();
+        double globalScaleV = data.inputValue(globalScale).asDouble();
+        double poleVectorLockV = data.inputValue(poleVectorLock).asDouble();
+        double stretchV = data.inputValue(stretch).asDouble();
+        double slideV = data.inputValue(slide).asDouble();
+
+        // Sum of bone init lengths
+        double chainInitLength = upInitLengthV + downInitLengthV;
+
+        MVector startVec(startMatrixV[3][0], startMatrixV[3][1], startMatrixV[3][2]);
+        MVector endVec(endMatrixV[3][0], endMatrixV[3][1], endMatrixV[3][2]);
+        MVector poleVec(poleVectorMatrixV[3][0], poleVectorMatrixV[3][1], poleVectorMatrixV[3][2]);
+
+        MVector startToEndVec = endVec - startVec;
+        MVector startToPoleVec = poleVec - startVec;
+        MVector poleToEndVec = endVec - poleVec;
+
+        double startToEndLength = startToEndVec.length();
+        double startToPoleLength = startToPoleVec.length();
+        double poleToEndLength = poleToEndVec.length();
+
+        double upScaleV = upInitLengthV;
+        double downScaleV = downInitLengthV;
+
+
+        // Apply slide
+        if (slideV < 0.5)
+        {
+            upScaleV *= slideV / 0.5;
+            downScaleV = chainInitLength - downScaleV;
+        }
+
+        if (slideV > 0.5)
+        {
+            downScaleV *= (1 - slideV) / 0.5;
+            upScaleV = chainInitLength - downScaleV;
+        }
+        //
+
+
+        // Apply stretch
+        if (chainInitLength < startToEndLength)
+        {
+            double scale = startToEndLength / chainInitLength;
+
+            // The simplest scaling
+            upScaleV *= 1.0 + (scale - 1.0) * stretchV;
+            downScaleV *= 1.0 + (scale - 1.0) * stretchV;
+        }
+        //
+
+
+        // Apply polar vector lock
+        if (poleVectorLockV > 0)
+        {
+            upScaleV += poleVectorLockV * (startToPoleLength - upScaleV);
+            downScaleV += poleVectorLockV * (poleToEndLength - downScaleV);
+        }
+        //
+
+        // Set output values
+        data.outputValue(upScale).set(upScaleV);
+        data.outputValue(upScale).setClean();
+
+        data.outputValue(downScale).set(downScaleV);
+        data.outputValue(downScale).setClean();
+    }
 
     return MStatus();
 }
